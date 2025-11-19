4496fae456a3e6ab53038c8f84299055fc5f4814
vandegn <vandegn@miamioh.edu>
2025-11-19 18:55:38 -0500

    trying to track UUID to respawn

--- src/main/java/rbd/ReturnByDeath.java
+++ src/main/java/rbd/ReturnByDeath.java
@@ -1,9 +1,23 @@
 package rbd;
 
 import net.fabricmc.api.ModInitializer;
-
+import rbd.*;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.PlayerRespawnLogic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class ReturnByDeath implements ModInitializer {
 	public static final String MOD_ID = "return-by-death";
@@ -13,12 +27,16 @@ public class ReturnByDeath implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// player spawn data captured from setSpawnPoint
+	private static final Map<UUID, RespawnLocation> LAST_SPAWN = new ConcurrentHashMap<>();
+	public record RespawnLocation(BlockPos pos, ResourceKey<Level> dimension, float angle, boolean forced) {}
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Proceed with mild caution
 
-//		LOGGER.info("Hello Fabric world!");
+		ExampleMixin.loadMap(LAST_SPAWN);
+//		LOGGER.info("Hello Fabic world!");
 	}
 }
--- src/main/java/rbd/mixin/ExampleMixin.java
+++ src/main/java/rbd/mixin/ExampleMixin.java
@@ -1,15 +1,34 @@
 package rbd.mixin;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import rbd.ReturnByDeath;
+import net.minecraft.server.*;
+import java.util.Map;
+import java.util.UUID;
+import net.minecraft.network.chat;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
+	public void ExampleMixin() {
+
+	}
 	@Inject(at = @At("HEAD"), method = "loadLevel")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadLevel()V
+	}
+
+	@Inject(at = @At("TAIL"), method = "<init>")
+	private void init(Map<UUID, ReturnByDeath.RespawnLocation> map) {
+
+		ServerPlayer player = (ServerPlayer)(Object)this;
+		UUID id = player.getUUID();
+		player.sendSystemMessage(Component.literal("player " + id + " respawn location is now: " + map.get(id)));
+
 	}
 }
